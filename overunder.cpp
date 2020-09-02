f27a5cdcd899746cd000942dcc1fae53e674d1a1
Felipe <fegomezl@unal.edu.co>
2020-09-02 14:10:43 -0500

    Se añadio un formato mas visible

--- overunder.cpp
+++ overunder.cpp
@@ -5,16 +5,21 @@ using namespace std;
 int main(int argc, char *argv[]) {
   //argc guarda el numero de terminos en la linea ejecutada
   //argv[] guarda en cada elemento el termino correspondiente de la linea
-  double under=1.0;
-  double over=1.0;
+
+  cout.precision(7);
+  cout.setf(ios::scientific);
+  
   int N=atoi(argv[1]);
 
+  double under=1.0;
+  double over=1.0;
+
   for(int i=0;i<N;i++){
-    under /= 2.0;
+   under /= 2.0;
     over *= 2.0;
-    cout <<i<<endl<<under<<endl<<over<<endl<<"-------------"<<endl;
-  }
-    
+    cout <<i<<"\t"<<under<<"\t"<<over<<endl<<"-----------------------------------------"<<endl;
+   }
+
   return 0;
 
 }
